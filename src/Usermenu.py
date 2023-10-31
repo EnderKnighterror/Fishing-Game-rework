62c632d4d5198c4c8691bcb276c9c23ec528d7e0
EnderKnightError <jarrodwilliams.1@outlook.com>
2023-10-31 16:03:13 +1030

    fixed login

--- src/Usermenu.py
+++ src/Usermenu.py
@@ -1,9 +1,53 @@
+import csv
+import hashlib
+import os
 import tkinter as tk
 from tkinter import messagebox
 
 root = tk.Tk()
 root.title("User management system")
 root.geometry("300x200")
+
+ASSETS_FOLDER = "users.csv"
+
+
+def hash_password(password):
+    """Hash the password using SHA256"""
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def save_user(username, hashed_password):
+    file_path = os.path.join(ASSETS_FOLDER, 'users.csv')
+
+    with open(file_path, "a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([username, hashed_password])
+
+
+def user_exists(username):
+    file_path = os.path.join(ASSETS_FOLDER, 'users.csv')
+
+    try:
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == username:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def authenticate_user(username, password):
+    file_path = os.path.join(ASSETS_FOLDER, 'users.csv')
+
+    hashed_password = hash_password(password)
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == username and row[1] == hashed_password:
+                return True
+    return False
 
 
 def register():
@@ -26,7 +70,7 @@ def login():
     if not user_exists(username):
         messagebox.showerror("Error", "User does not exist.")
         return
-    if not authenticate_user(Username, password):
+    if not authenticate_user(username, password):
         messagebox.showerror("Error", "Incorrect password")
         return
     messagebox.showinfo("Success", "Login Successful")
@@ -44,6 +88,9 @@ username_entry.pack(pady=5)
 password_entry = tk.Entry(root)
 password_entry.pack(pady=5)
 
+password_entry = tk.Entry(root, show="*")
+password_entry.pack(pady=5)
+
 register_button = tk.Button(root, text="Register", command=register)
 register_button.pack(pady=5)
 
@@ -52,3 +99,5 @@ login_button.pack(pady=5)
 
 exit_button = tk.Button(root, text="Exit", command=root.quit)
 exit_button.pack(pady=5)
+
+root.mainloop()
